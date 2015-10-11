48a6cc08e9e859f3c1045dff4e6ebc4b74cd8d26
jbbodart <jbbodart@yahoo.com>
2015-10-11 18:49:48 +0200

    Change conf dir to /config/deluge/

--- apps/nobody/deluge.sh
+++ apps/nobody/deluge.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-if [[ -f /config/core.conf ]]; then
+if [[ -f /config/deluge/core.conf ]]; then
 	# reset listen interface ip address for deluge
-	sed -i -e 's/"listen_interface".*/"listen_interface": "",/g' /config/core.conf
+	sed -i -e 's/"listen_interface".*/"listen_interface": "",/g' /config/deluge/core.conf
 fi
 
 # if vpn set to "no" then set deluge to random incoming port
@@ -21,4 +21,4 @@ fi
 echo "[info] All checks complete, starting Deluge daemon..."
 
 # run deluge daemon
-/usr/bin/deluged -d -c /config -L info -l /config/deluged.log
+/usr/bin/deluged -d -c /config/deluge -L info -l /config/deluge/deluged.log
