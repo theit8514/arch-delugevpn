98eb83b0a006c9986690665a3b37b87379ae943c
jbbodart <jbbodart@yahoo.com>
2015-10-12 22:30:01 +0200

    Set up data dirs

--- apps/root/config.sh
+++ apps/root/config.sh
@@ -1,9 +1,20 @@
 #!/bin/bash
 
+# set up config directory
+#########################
+
 mkdir -p /config/openvpn
 mkdir -p /config/privoxy
 mkdir -p /config/deluge
 
 chown -R nobody:users /config/privoxy /config/deluge
-chmod -R 775 /config/privoxy /config/deluge
+#chmod -R 775 /config/privoxy /config/deluge
 
+# set up data directory
+#########################
+
+mkdir -p /data/Downloads
+mkdir -p /data/Torrents
+mkdir -p /data/Seeding
+
+chown -R nobody:users /data/Downloads /data/Torrents /data/Seeding
