98e0ca167712a0c9e518411b6f857ab466ddd51e
jbbodart <jbbodart@yahoo.com>
2015-10-13 22:02:35 +0200

    Update deluge.sh

--- apps/nobody/deluge.sh
+++ apps/nobody/deluge.sh
@@ -1,21 +1,17 @@
 #!/bin/bash
 
+echo "[info] starting Deluge..."
+
 if [[ -f /config/deluge/core.conf ]]; then
 	# reset listen interface ip address for deluge
 	sed -i -e 's/"listen_interface".*/"listen_interface": "",/g' /config/deluge/core.conf
 fi
 
 # if vpn set to "no" then set deluge to random incoming port
-if [[ $VPN_ENABLED == "no" ]]; then
-	echo "[info] VPN not enabled, skipping configuration of Deluge"
-else
-	echo "[info] VPN enabled, configuring Deluge..."
-
+if [[ $VPN_ENABLED == "yes" ]]; then
+	echo "[info] VPN enabled, waiting for tun0 interface to come up..."
 	# run script to check ip is valid for tun0
 	source /home/nobody/checkip.sh
-
-	# run scripts to configure deluge ip and port
-	source /home/nobody/setip.sh & source /home/nobody/setport.sh &
 fi
 
 echo "[info] All checks complete, starting Deluge daemon..."
