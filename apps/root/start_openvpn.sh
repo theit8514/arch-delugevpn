cbb2dee9b2d064f54b31a7dea834645fa4b2bb88
jbbodart <jbbodart@yahoo.com>
2015-10-17 22:40:03 +0200

    Restore VPN_CONFIG variable

--- apps/root/start_openvpn.sh
+++ apps/root/start_openvpn.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
-/usr/bin/openvpn --cd /config/openvpn --config "$VPN_CONFIG" --mute-replay-warnings --up /root/update-resolv-conf.sh --down /root/update-resolv-conf.sh --script-security 2 --keepalive 10 60
+VPN_CONFIG=$(find /config/openvpn -maxdepth 1 -name "*.ovpn" -print)
+/usr/bin/openvpn --cd /config/openvpn --config "${VPN_CONFIG}" --mute-replay-warnings --up /root/update-resolv-conf.sh --down /root/update-resolv-conf.sh --script-security 2 --keepalive 10 60
