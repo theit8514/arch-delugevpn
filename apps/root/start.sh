e1008ec38c504e0a700c1fbc75eca1a567cbd323
jbbodart <jbbodart@yahoo.com>
2015-10-17 09:50:38 +0200

    Update and rename config.sh to start.sh

--- apps/root/start.sh
+++ apps/root/start.sh
@@ -13,9 +13,19 @@ chown -R nobody:users /config/privoxy /config/deluge
 # set up data directory
 #########################
 
-mkdir -p /data/Downloads
-mkdir -p /data/Torrents
-mkdir -p /data/Seed
-mkdir -p /data/Watch
+mkdir -p /data/downloads
+mkdir -p /data/torrents
+mkdir -p /data/seed
+mkdir -p /data/watch
 
 chown -R nobody:users /data/Downloads /data/Torrents /data/Seed /data/Watch
+
+# set up sshd
+#############
+
+if [[ $SSHD_ENABLED == "yes" ]]; then
+    mkdir -p /root/.ssh
+    chmod 700 /root/.ssh
+fi
+
+
