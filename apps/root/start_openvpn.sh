a0a6ab9865757a067b63bc3e4ca8af5ff44797de
jbbodart <jbbodart@yahoo.com>
2015-10-17 19:17:05 +0200

    Remove useless test

--- apps/root/start_openvpn.sh
+++ apps/root/start_openvpn.sh
@@ -1,5 +1,3 @@
 #!/bin/bash
 
-if [[ "${VPN_ENABLED}" == "yes" ]]; then
-  /usr/bin/openvpn --cd /config/openvpn --config "$VPN_CONFIG" --mute-replay-warnings --up /root/update-resolv-conf.sh --down /root/update-resolv-conf.sh --script-security 2 --keepalive 10 60
-fi
+/usr/bin/openvpn --cd /config/openvpn --config "$VPN_CONFIG" --mute-replay-warnings --up /root/update-resolv-conf.sh --down /root/update-resolv-conf.sh --script-security 2 --keepalive 10 60
